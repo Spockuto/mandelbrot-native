619d4969ac2d1079fe6e0ed209f6610ee9330fa8
Spockuto <venkythesupersaiyan@gmail.com>
2024-06-28 21:32:09 +0100

    v1

---
+++ README.md
@@ -0,0 +1,12 @@
+mandelbrot-native
+=================
+
+A native mandelbrot implementation with optimized escape, smooth coloring and naive parallelization with `rayon`.
+
+To run the generator,
+```
+cargo run --release
+```
+
+## Tweaking
+FAFO
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,72 @@
-use colors_transform::Color;
-use colors_transform::{Hsl, Rgb};
 use image::ImageBuffer;
-use num_complex::Complex;
+use piston_window::{
+    EventLoop, EventSettings, Events, PistonWindow, Texture, TextureContext, TextureSettings,
+    WindowSettings,
+};
+use std::f64::consts::LN_2;
+
+mod constants;
+use constants::{CENTER, HEIGHT, ITERATIONS, PALETTE, WIDTH, ZOOM_FACTOR};
+
+fn scale_x(x: u32, width: u32, min_re: f64, max_re: f64, zoom: f64, center_re: f64) -> f64 {
+    center_re + (x as f64 - width as f64 / 2.0) * (max_re - min_re) / (width as f64 * zoom)
+}
+
+fn scale_y(y: u32, height: u32, min_im: f64, max_im: f64, zoom: f64, center_im: f64) -> f64 {
+    center_im - (y as f64 - height as f64 / 2.0) * (max_im - min_im) / (height as f64 * zoom)
+}
+
+fn interpolate_color(color1: (u8, u8, u8), color2: (u8, u8, u8), t: f64) -> (u8, u8, u8) {
+    let r = (color1.0 as f64 * (1.0 - t) + color2.0 as f64 * t) as u8;
+    let g = (color1.1 as f64 * (1.0 - t) + color2.1 as f64 * t) as u8;
+    let b = (color1.2 as f64 * (1.0 - t) + color2.2 as f64 * t) as u8;
+    (r, g, b)
+}
 
 fn mandelbrot_zoom_frame(
     width: u32,
     height: u32,
     iterations: u32,
-    hue: f32,
-    color: [u8; 4],
     zoom: f64,
     center_x: f64,
     center_y: f64,
 ) -> ImageBuffer<image::Rgba<u8>, Vec<u8>> {
-    let scale_x = 5.0 / width as f64 / zoom;
-    let scale_y = 5.0 / height as f64 / zoom;
+    let palette_size = PALETTE.len() as u32;
 
     ImageBuffer::from_par_fn(width, height, |x, y| {
-        let temp_x = (x as f64 * scale_x) - 2.5 / zoom + center_x;
-        let temp_y = (y as f64 * scale_y) - 2.5 / zoom + center_y;
-
-        let z = Complex::new(temp_x, temp_y);
-        let mut cz = z;
+        let x0 = scale_x(x, width, -2.5, 1.0, zoom, center_x);
+        let y0 = scale_y(y, height, -1.0, 1.0, zoom, center_y);
+        let (mut z_x, mut z_y, mut x2, mut y2) = (0.0, 0.0, 0.0, 0.0);
+        let mut color = (iterations % palette_size) as f64;
 
         for i in 0..iterations {
-            cz = cz * cz + z;
+            z_y = (z_x + z_x) * z_y + y0;
+            z_x = x2 - y2 + x0;
+            x2 = z_x * z_x;
+            y2 = z_y * z_y;
 
-            if cz.norm_sqr() > 5.0 {
-                let color_string = Hsl::from(
-                    hue,
-                    100_f32,
-                    ((i as f32) / (iterations as f32) * 100.0) as f32,
-                );
-                return image::Rgba([
-                    color_string.get_red().round() as u8,
-                    color_string.get_green().round() as u8,
-                    color_string.get_blue().round() as u8,
-                    255,
-                ]);
+            if x2 + y2 > (1 << 16) as f64 {
+                let log_zn = (x2 + y2).log((1 << 8) as f64) / 2.0;
+                let nu = (log_zn / LN_2).log((1 << 8) as f64) / LN_2;
+                color = (i as f64 + 1.0 - nu) % palette_size as f64;
+                break;
             }
         }
 
-        return image::Rgba(color);
+        let i1 = color.floor() as usize;
+        let i2 = (i1 + 1) % palette_size as usize;
+        let (r, g, b) = interpolate_color(PALETTE[i1], PALETTE[i2], color - i1 as f64);
+
+        image::Rgba([r, g, b, 128])
     })
 }
 
 fn main() {
-    use piston_window::*;
-
-    let height: u32 = 1080;
-    let width: u32 = 1920;
-    let color_string = "#EFEFEF";
-    let iterations = 500;
-    let hue = 30.0;
     let mut zoom = 1.0;
-    let center = 6;
+    let center = 3;
+    let (center_x, center_y) = CENTER[center];
 
-    let (center_x, center_y) = match center {
-        1 => (-0.75, 0.1),
-        2 => (0.339410819995598, -0.050668285162643),
-        3 => (-0.10109636384562, 0.95628651080914),
-        4 => (-0.77568377, 0.13646737),
-        5 => ( 0.272149607027528, 0.005401159465460),
-        6 => (-1.7492046334590113301, 0.00028684660234660531403),
-        7 => (0.2925755, -0.0149977),
-        8 => (-0.814158841137593, 0.189802029306573),
-        9 => (-0.1182402951560276787014475129283, 0.64949165134945441813936036487738),
-        _ => (1.0, 1.0),
-    };
-
-    let mut window: PistonWindow = WindowSettings::new("mandlebrot set", [width, height])
+    let mut window: PistonWindow = WindowSettings::new("mandlebrot set", [WIDTH, HEIGHT])
         .build()
         .unwrap();
 
@@ -78,24 +75,15 @@ fn main() {
         encoder: window.factory.create_command_buffer().into(),
     };
 
-    let mut default: [u8; 4] = [0; 4];
-    let default_color = Rgb::from_hex_str(color_string).unwrap();
-    default[0] = default_color.get_red() as u8;
-    default[1] = default_color.get_green() as u8;
-    default[2] = default_color.get_blue() as u8;
-    default[3] = 255;
-
     let mut events = Events::new(EventSettings::new().lazy(false));
+    let settings = TextureSettings::new();
     while let Some(e) = events.next(&mut window) {
         window.draw_2d(&e, |c, g, device| {
-            zoom += 10000.0;
-            let data = mandelbrot_zoom_frame(
-                width, height, iterations, hue, default, zoom, center_x, center_y,
-            );
+            zoom += ZOOM_FACTOR;
+            let data = mandelbrot_zoom_frame(WIDTH, HEIGHT, ITERATIONS, zoom, center_x, center_y);
 
-            let texture =
-                Texture::from_image(&mut texture_context, &data, &TextureSettings::new()).unwrap();
-            image(&texture, c.transform, g);
+            let texture = Texture::from_image(&mut texture_context, &data, &settings).unwrap();
+            piston_window::image(&texture, c.transform, g);
             texture_context.encoder.flush(device);
         });
     }
